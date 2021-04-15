ffc60c59490d1b1646f7649c83fa372d9f5697e2
andrevis <andre.vis@outlook.com>
2021-04-15 21:48:33 +0200

    working on getPossibleStonesForCoordinate

--- source/Predictor.ts
+++ source/Predictor.ts
@@ -1,6 +1,6 @@
 import { State } from './State'
 import { Stone } from './Stone'
-import { ColorShape, Colors, Shapes, Coordinate } from './Types'
+import { ColorShape, Colors, Shapes, Coordinate, Direction } from './Types'
 
 export class Predictor {
     private possibleStones: { [key in `${Colors}${Shapes}`]: Array<Coordinate> }
@@ -9,17 +9,21 @@ export class Predictor {
     constructor(state: State) {
         this.state = state;
 
-        this.updateCache()
+        this.startCache()
         this.state.addEventListener('turn-added', (event: Event) => {
             const turn = (event as CustomEvent).detail
             this.updateCache(turn.stones)
         })
     }
 
+    startCache () {
+        // check for every this.state.borderCoordinates which stones are possible
+    }
 
-
+    /**
+     * Stones may be empty as the player can change one or multiple stone in a turn instead of playing stones.
+     */
     updateCache (stones: Array<Stone> = []) {
-        
 
         // Determine which coordinates should be recalculated.
         // calculate possible ColorShapes for a specific coordinate.
@@ -29,6 +33,42 @@ export class Predictor {
         // output one or multiple stones that give the best score.
     }
     
+    getPossibleStonesForCoordinate (coordinate: Coordinate) {
+        const [ x, y ] = coordinate
+        const neighbourStones = [
+            [x, y - 1],
+            [x + 1, y],
+            [x, y + 1],
+            [x - 1, y]
+        ].filter((coordinate: Coordinate) => this.state.stonesCoordinates.has(coordinate.join(',')))
+        .map(coordinate => this.state.stones.find((stone: Stone) => stone.coordinates(true) === coordinate.join(',')))
+        
+        const bars = neighbourStones.map(neighbourStone => {
+            const direction = neighbourStone.x === coordinate[0] ? Direction.Vertical : Direction.Horizontal
+            return neighbourStone.bar(direction)
+        })
+
+        for (const bar of bars) {
+            const isSameColor = bar[0].color === bar[1].color
+            // create the barQwirkle array containing all the shapes from one color or all the colors one shape and compare with bar
+            // return the stones present in all bars
+            const barQwirkle = []
+        }
+
+        /*
+                 | |
+            ___  |_|_______
+            ___| |X|_|______
+                 | |
+                 | |
+        */
+        //get all connected bars (max 4)
+        //check if bars are valid
+
+        // bar [rood vierkant, rood rondje] Diff: []
+        //  
+    }
+
     getPossibleTurns () {
 
     }
--- tests/Predictor.test.ts
+++ tests/Predictor.test.ts
@@ -8,35 +8,7 @@ const testState = new State()
 testState.setInitial(referenceState1)
 const predictor = new Predictor(testState);
 
-test('predictor returns the best turn', () => {
-  // const bestTurn = predictor.getBestPossibleTurns([
-  //   [Colors.Red, Shapes.Circle],
-  //   // [Colors.Red, Shapes.Circle],
-  //   // [Colors.Green, Shapes.Circle],
-  //   // [Colors.Purple, Shapes.Circle],
-  //   // [Colors.Blue, Shapes.Square],
-  //   // [Colors.Blue, Shapes.Diamond],
-  // ])
-  
-
-
-
-
+test('getPossibleStonesForCoordinate', () => {
+  const possibleStones = predictor.getPossibleStonesForCoordinate([0, -1])
+  console.log(possibleStones)
 })
-// test('getBorderCoordinates returns the correct border coordinates', () => {
-//     const testState = new State(referenceState2);
-//     const predictor = new Predictor(testState);   
-//     const border = predictor.getBorderCoordinates(testState.stones).sort(sortCoordinates)
-
-//     const expectedBorder: Array<Coordinate> = [
-//         <Coordinate> [1,0],
-//         <Coordinate> [-1,0],
-//         <Coordinate> [-1,1],
-//         <Coordinate> [1,1],
-//         <Coordinate> [0,-1],
-//         <Coordinate> [0,2],
-        
-//     ].sort(sortCoordinates)
-    
-//     expect(border).toEqual(expectedBorder)
-// })
