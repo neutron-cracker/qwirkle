71a45d6424192a7d36b6079defa8f143c21320dc
Daniel Beeke (NL) <daniel.beeke@om.org>
2021-07-21 21:27:47 +0200

    finishing svg work

--- source/QwirkleBoard.ts
+++ source/QwirkleBoard.ts
@@ -3,12 +3,12 @@ import { svg, html, render } from 'uhtml/async'
 import { Colors } from './Types'
 
 const colorMap = {
-  'p': 'purple',
-  'r': 'red',
-  'b': 'blue',
-  'y': 'yellow',
-  'g': 'green',
-  'o': 'orange',
+  'p': 'rgb(135, 41, 150)',
+  'r': 'rgb(255, 46, 23)',
+  'b': 'rgb(0, 144, 211)',
+  'y': 'rgb(255, 217, 57)',
+  'g': 'rgb(136, 201, 70)',
+  'o': 'rgb(255, 144, 39)',
 }
 
 const factor = .7
@@ -16,12 +16,11 @@ const flowerFactor = .33
 const innerQuadrogramFactor = .4
 const outerQuadrogramFactor = .1
 const diamondFactor = .15
-const innerOctagramFactor = innerQuadrogramFactor
-const outerOctagramFactor = outerQuadrogramFactor
+const stoneFactor = .02
 
 const shapeMap = {
-  1: (x, y, color) => svg`<circle fill=${colorMap[color]} r=${factor / 1.9} cx=${x + .5} cy=${y + .5} />`,
-  2: (x, y, color) => svg`<rect fill=${colorMap[color]} x=${x + 0.5 - factor / 2} y=${y + 0.5 - factor / 2} width=${factor} height=${factor} />`,
+  1: (x, y, color) => svg`<rect fill=${colorMap[color]} x=${x + 0.5 - factor / 2} y=${y + 0.5 - factor / 2} width=${factor} height=${factor} />`,
+  2: (x, y, color) => svg`<circle fill=${colorMap[color]} r=${factor / 1.9} cx=${x + .5} cy=${y + .5} />`,
   3: (x, y, color) => svg`
     <circle fill=${colorMap[color]} r=${factor / 4} cx=${x + 1 -flowerFactor} cy=${y + .5} />
     <circle fill=${colorMap[color]} r=${factor / 4} cx=${x + flowerFactor} cy=${y + .5} />
@@ -49,16 +48,30 @@ export class QwirkleBoard extends HTMLElement {
 
   private state: Array<any> 
 
+  private smallestX = 0
+  private smallestY = 0
+  private biggestX = 0
+  private biggestY = 0
+
+
   connectedCallback () {
     this.state = JSON.parse(this.getAttribute('state'))
+
+    for (const [x, y] of this.state) {
+      if (x < this.smallestX) this.smallestX = x
+      if (x > this.biggestX) this.biggestX = x
+      if (y < this.smallestY) this.smallestY = y
+      if (y > this.biggestY) this.biggestY = y
+    }
+
     this.draw()
   }
 
   draw () {
     render(this, svg`
-      <svg viewBox="-3 -3 10 10" xmlns="http://www.w3.org/2000/svg">
+      <svg style="transform: scale(1, -1)" viewBox=${`${this.smallestX} ${this.smallestY} ${this.biggestX - this.smallestX + 1} ${this.biggestY - this.smallestY + 1}`} xmlns="http://www.w3.org/2000/svg">
         ${this.state.map(([x, y, color, shape]) => svg`
-          <rect x=${x} y=${y} width="1" height="1" />
+          <rect fill="rgb(44, 46, 53)" x=${x + stoneFactor} y=${y + stoneFactor} width=${1 - stoneFactor * 2} height=${1 - stoneFactor * 2} />
           ${shapeMap[shape](x, y, color)}
         `)}
       </svg>
